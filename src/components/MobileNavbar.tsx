8424a91ad5de27b360b930b9e94276d74d673b7f
Joseph Mark <josephmarkmay@gmail.com>
2025-06-26 12:49:44 +0300

    updated sidebar for mobile view

--- src/components/MobileNavbar.tsx
+++ src/components/MobileNavbar.tsx
@@ -18,7 +18,7 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet';
 import { useState } from 'react';
-import { useAuth, SignInButton, SignOutButton } from '@clerk/nextjs';
+import { useAuth, SignInButton, SignOutButton, useUser } from '@clerk/nextjs';
 import { useTheme } from 'next-themes';
 import Link from 'next/link';
 
@@ -26,6 +26,12 @@ function MobileNavbar() {
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const { isSignedIn } = useAuth();
   const { theme, setTheme } = useTheme();
+  const { user } = useUser();
+
+  if (!user) return null;
+
+  const username =
+    user.username ?? user.emailAddresses[0].emailAddress.split('@')[0];
 
   return (
     <div className='flex md:hidden items-center space-x-2'>
@@ -79,7 +85,7 @@ function MobileNavbar() {
                   className='flex items-center gap-3 justify-start'
                   asChild
                 >
-                  <Link href='/profile'>
+                  <Link href={`/profile/${username}`}>
                     <UserIcon className='w-4 h-4' />
                     Profile
                   </Link>
